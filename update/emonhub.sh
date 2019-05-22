12eb8a4fded9684f43a6a86068e2a1b2472fc4c4
Trystan Lea <trystan.lea@gmail.com>
2019-05-22 13:43:43 +0100

    emonhub.service is now symlinked again

--- update/emonhub.sh
+++ update/emonhub.sh
@@ -14,34 +14,10 @@ if [ -d $usrdir/emonhub ]; then
     git status
     git pull
     
-    # ---------------------------------------------------------
-    # Update service
-    # ---------------------------------------------------------
-    service=emonhub
-    emonhub_src_path=$usrdir/emonhub/src
-    emonhub_conf_path=$usrdir/data
-    emonhub_logfile_path=/var/log/emonhub
+    # can be used to change service source location in future
+    # sudo ln -sf $usrdir/emonhub/service/emonhub.service.new /lib/systemd/system/emonhub.service
     
-    if [ ! -d /var/log/emonhub ]; then
-        sudo mkdir $emonhub_logfile_path
-        sudo chown emonhub $emonhub_logfile_path
-    fi
-
-    if [ -f /lib/systemd/system/$service.service ]; then
-        echo "- reinstalling $service.service"
-        sudo systemctl stop $service.service
-        sudo systemctl disable $service.service
-        sudo rm /lib/systemd/system/$service.service
-    else
-        echo "- installing $service.service"
-    fi
-
-    sudo cp $usrdir/emonhub/service/$service.service /lib/systemd/system
-    # Set ExecStart path to point to installed script and config location
-    sudo sed -i "s~ExecStart=.*~ExecStart=$emonhub_src_path/emonhub.py --config-file=$emonhub_conf_path/emonhub.conf --logfile=$emonhub_logfile_path/emonhub.log~" /lib/systemd/system/$service.service
-    sudo systemctl enable $service.service
     sudo systemctl restart $service.service
-
     state=$(systemctl show $service | grep ActiveState)
     echo "- Service $state"
     # ---------------------------------------------------------
