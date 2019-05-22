eeec6bd31f705385434b8426d8d655b3cb636146
Trystan Lea <trystan.lea@gmail.com>
2019-05-22 13:08:34 +0100

    fix emonhub service update

--- update/emonhub.sh
+++ update/emonhub.sh
@@ -14,16 +14,38 @@ if [ -d $usrdir/emonhub ]; then
     git status
     git pull
     
-    if [ ! -d /var/log/emonhub ]; then
-        sudo mkdir /var/log/emonhub
-        sudo chown emonhub:emonhub /var/log/emonhub
-    fi
+    # ---------------------------------------------------------
+    # Update service
+    # ---------------------------------------------------------
+    service=emonhub
+    emonhub_src_path=$usrdir/emonhub/src
+    emonhub_conf_path=$usrdir/data
+    emonhub_logfile_path=/var/log/emonhub
     
-    service="emonhub"
-    servicepath="$usrdir/emonhub/service/emonhub.service"
-    $usrdir/EmonScripts/common/install_emoncms_service.sh $servicepath $service
+    if [ ! -d /var/log/emonhub ]; then
+        sudo mkdir $emonhub_logfile_path
+        sudo chown emonhub $emonhub_logfile_path
+    fi
 
-    # echo
+    if [ -f /lib/systemd/system/$service.service ]; then
+        echo "- reinstalling $service.service"
+        sudo systemctl stop $service.service
+        sudo systemctl disable $service.service
+        sudo rm /lib/systemd/system/$service.service
+    else
+        echo "- installing $service.service"
+    fi
+
+    sudo cp $usrdir/emonhub/service/$service.service /lib/systemd/system
+    # Set ExecStart path to point to installed script and config location
+    sudo sed -i "s~ExecStart=.*~ExecStart=$emonhub_src_path/emonhub.py --config-file=$emonhub_conf_path/emonhub.conf --logfile=$emonhub_logfile_path/emonhub.log~" /lib/systemd/system/$service.service
+    sudo systemctl enable $service.service
+    sudo systemctl restart $service.service
+
+    state=$(systemctl show $service | grep ActiveState)
+    echo "- Service $state"
+    # ---------------------------------------------------------
+    
     # echo "Running emonhub automatic node addition script"
     # $usrdir/emonhub/conf/nodes/emonpi_auto_add_nodes.sh $usrdir
 
