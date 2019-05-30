09134af283fccbb4b4685a557819746c995b8531
TrystanLea <glyn.hudson@openenergymonitor.org>
2019-05-30 23:13:02 +0100

    use emoncms virtual host

--- install/apache.sh
+++ install/apache.sh
@@ -8,22 +8,7 @@ sudo apt-get install -y apache2
 
 # Enable apache mod rewrite
 sudo a2enmod rewrite
-sudo cat <<EOF >> /tmp/emoncms.conf
-<Directory /var/www/html/emoncms>
-    Options FollowSymLinks
-    AllowOverride All
-    DirectoryIndex index.php
-    Order allow,deny
-    Allow from all
-</Directory>
-EOF
-sudo mv /tmp/emoncms.conf /etc/apache2/sites-available/emoncms.conf
-# Review is this line needed? if so check for existing entry
-# printf "ServerName localhost" | sudo tee -a /etc/apache2/apache2.conf 1>&2
+sudo cp $usrdir/EmonScripts/defaults/emoncms.conf /etc/apache2/sites-available/emoncms.conf
+sudo a2dissite 000-default.conf
 sudo a2ensite emoncms
-
-# Disable apache2 access logs
-sudo sed -i "s/^\tCustomLog/\t#CustomLog/" /etc/apache2/sites-available/000-default.conf
-sudo sed -i "s/^CustomLog/#CustomLog/" /etc/apache2/conf-available/other-vhosts-access-log.conf
-
 sudo systemctl restart apache2
