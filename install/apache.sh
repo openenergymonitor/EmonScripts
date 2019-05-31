aec892f43c7ba84ca038a7fdd1994dd259cf134c
TrystanLea <glyn.hudson@openenergymonitor.org>
2019-05-31 15:00:41 +0100

    apache logging

--- install/apache.sh
+++ install/apache.sh
@@ -6,9 +6,12 @@ echo "Apache configuration"
 echo "-------------------------------------------------------------"
 sudo apt-get install -y apache2
 
+sudo sed -i "s/^CustomLog/#CustomLog/" /etc/apache2/conf-available/other-vhosts-access-log.conf
+
 # Enable apache mod rewrite
 sudo a2enmod rewrite
 sudo cp $usrdir/EmonScripts/defaults/apache2/emoncms.conf /etc/apache2/sites-available/emoncms.conf
 sudo a2dissite 000-default.conf
 sudo a2ensite emoncms
 sudo systemctl restart apache2
+
