db522ad36768e46a14fe993963dc34229b1cfaf0
Trystan Lea <trystan.lea@googlemail.com>
2019-05-31 14:17:58 +0100

    Update apache.sh

--- install/apache.sh
+++ install/apache.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y apache2
 
 # Enable apache mod rewrite
 sudo a2enmod rewrite
-sudo cp $usrdir/EmonScripts/defaults/emoncms.conf /etc/apache2/sites-available/emoncms.conf
+sudo cp $usrdir/EmonScripts/defaults/apache2/emoncms.conf /etc/apache2/sites-available/emoncms.conf
 sudo a2dissite 000-default.conf
 sudo a2ensite emoncms
 sudo systemctl restart apache2
