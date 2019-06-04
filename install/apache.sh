4c0d38f431b3d80d444694c37f9b4f305023c794
TrystanLea <glyn.hudson@openenergymonitor.org>
2019-06-04 15:19:06 +0100

    move default to conf-available

--- install/apache.sh
+++ install/apache.sh
@@ -10,8 +10,15 @@ sudo sed -i "s/^CustomLog/#CustomLog/" /etc/apache2/conf-available/other-vhosts-
 
 # Enable apache mod rewrite
 sudo a2enmod rewrite
+
+# Default apache2 configuration
+sudo cp $usrdir/EmonScripts/defaults/apache2/emonsd.conf /etc/apache2/conf-available/emonsd.conf
+sudo a2enconf emonsd.conf
+
+# Configure virtual host
 sudo cp $usrdir/EmonScripts/defaults/apache2/emoncms.conf /etc/apache2/sites-available/emoncms.conf
 sudo a2dissite 000-default.conf
 sudo a2ensite emoncms
+
 sudo systemctl restart apache2
 
