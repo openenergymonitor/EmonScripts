72befcd7d8cd67cba33e9e0f4e2111636436c821
Trystan Lea <trystan.lea@googlemail.com>
2019-06-19 14:16:57 +0100

    Update init.sh

--- install/init.sh
+++ install/init.sh
@@ -8,6 +8,10 @@ sudo apt-get install -y git-core
 
 sudo mkdir $openenergymonitor_dir
 sudo chown $user $openenergymonitor_dir
+
+sudo mkdir $emoncms_dir
+sudo chown $user $openenergymonitor_dir
+
 cd $openenergymonitor_dir
 
 git clone https://github.com/openenergymonitor/EmonScripts.git
