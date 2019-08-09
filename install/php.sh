dda7495314b33d2b6cc2ec7116ba02f4050d8be4
Trystan Lea <trystan.lea@googlemail.com>
2019-08-09 10:50:48 +0100

    Update php.sh

--- install/php.sh
+++ install/php.sh
@@ -17,7 +17,8 @@ if [ "$install_mysql" = true ]; then
     sudo apt-get install -y php-mysql
 fi
 
-sudo apt-get install -y php-gd php$PHP_VER-opcache php-curl php-pear php-dev php-mcrypt php-common php-mbstring
+# sudo apt-get install -y php-gd php$PHP_VER-opcache php-curl php-pear php-dev php-mcrypt php-common php-mbstring
+sudo apt-get install -y php-gd php-curl php-pear php-dev php-common php-mbstring
 
 sudo pecl channel-update pecl.php.net
 
