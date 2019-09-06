5364e8640f4f7c6dcbde73f109c594d5bea507bd
Trystan Lea <trystan.lea@googlemail.com>
2019-09-06 16:28:54 +0100

    Update emonsd.sh

--- install/emonsd.sh
+++ install/emonsd.sh
@@ -47,18 +47,13 @@ sudo apt-get install -y ufw
 # sudo ufw allow 1883/tcp #(optional, Mosquitto)
 # sudo ufw enable
 
-# Review: Memory Tweak
-# Append gpu_mem=16 to /boot/config.txt this caps the RAM available to the GPU. 
-# Since we are running headless this will give us more RAM at the expense of the GPU
-# gpu_mem=16
+# Underclock and memory tweak
+# change arm_freq to 1200 and gpu_mem to 16
+# more ram for general purpose, less for GPU
+sudo sed -i "s/^#arm_freq=800/arm_freq=1200\ngpu_mem=16/" /boot/config.txt
 
-# Review: change elevator=deadline to elevator=noop
-# sudo nano /boot/cmdline.txt
-# see: https://github.com/openenergymonitor/emonpi/blob/master/docs/SD-card-build.md#raspi-serial-port-setup
-
-# Review: Force NTP update 
-# is this needed now that image is not read only?
-# 0 * * * * /home/pi/emonpi/ntp_update.sh >> /var/log/ntp_update.log 2>&1
+# 6 Sep 2019 decision to leave elevator setting as default
+# option to review in future: elevator=noop
 
 # Review automated install: Emoncms Language Support
 # sudo dpkg-reconfigure locales
