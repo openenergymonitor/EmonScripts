08afb079427219364058b07797a73535e7c43622
TrystanLea <glyn.hudson@openenergymonitor.org>
2019-09-17 14:01:40 +0100

    gitignore config.ini error message if config.ini does not exist rearrange config.ini default

--- install/load_config.sh
+++ install/load_config.sh
@@ -2,4 +2,10 @@
 # Do not change these lines, they are used to auto detect the installation location
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # usrdir=${DIR/\/EmonScripts\/install/}
-source config.ini
+
+if [ -f config.ini ]; then
+    source config.ini
+else
+    echo "config.ini does not exist, please create from default e.g emonsd.config.ini"
+    exit 0
+fi
