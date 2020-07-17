c27eb950a91d624745d76d8ad34cdf707848da32
Adrian Minde <Adrian_Minde@live.de>
2020-07-17 09:49:46 +0200

    Fix non emonsd oem path and make logrotate more generic

--- install/non_emonsd.sh
+++ install/non_emonsd.sh
@@ -5,5 +5,5 @@ source load_config.sh
 # Install custom logrotate
 # --------------------------------------------------------------------------------
 sudo ln -sf $openenergymonitor_dir/EmonScripts/defaults/etc/logrotate.d/emoncms-non-emonsd /etc/logrotate.d/emoncms
+sudo chmod 644 $openenergymonitor_dir/EmonScripts/defaults/etc/logrotate.d/emoncms-non-emonsd
 sudo chown root /etc/logrotate.d/emoncms
-sudo chmod 644 /opt/openenergymonitor/EmonScripts/defaults/etc/logrotate.d/emoncms-non-emonsd
