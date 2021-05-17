d7dfb7dacced98a2a8d9a2ace037148578827638
TrystanLea <trystan.lea@gmail.com>
2021-05-17 13:21:42 +0100

    remove update of old settings.php file - no longer used

--- update/emoncms_core.sh
+++ update/emoncms_core.sh
@@ -12,28 +12,6 @@ if [ ! -d $emoncms_www ]; then
 fi
 
 # -----------------------------------------------------------------
-# Record current state of emoncms settings.php
-# This needs to be run prior to emoncms git pull
-# -----------------------------------------------------------------
-cd
-echo
-if [ -f $emoncms_www/settings.php ]; then
-  current_settings_md5="$($openenergymonitor_dir/EmonScripts/common/./md5sum.py $emoncms_www/settings.php)"
-  echo "current settings.php md5: $current_settings_md5"
-
-  current_default_settings_md5="$($openenergymonitor_dir/EmonScripts/common/md5sum.py $emoncms_www/default.emonpi.settings.php)"
-  echo "Default settings.php md5: $current_default_settings_md5"
-
-  if [ "$current_default_settings_md5" == "$current_settings_md5" ]; then
-    echo "settings.php has NOT been user modifed"
-    settings_unmodified=true
-  else
-    echo "settings.php HAS been user modified"
-    settings_unmodified=false
-  fi
-fi
-
-# -----------------------------------------------------------------
 # Pulling in latest Emoncms changes
 # -----------------------------------------------------------------
 echo
@@ -54,28 +32,6 @@ else
     echo
     git status
     echo
-fi
-
-# -----------------------------------------------------------------
-# check to see if user has modifed settings.php and if update is need. Auto apply of possible
-# -----------------------------------------------------------------
-echo
-if [ -f $emoncms_www/settings.php ]; then
-  new_default_settings_md5="$($openenergymonitor_dir/EmonScripts/common/md5sum.py $emoncms_www/default.emonpi.settings.php)"
-  echo "NEW default settings.php md5: $new_default_settings_md5"
-
-  # check to see if there is an update waiting for settings.php
-  if [ "$new_default_settings_md5" != "$current_default_settings_md5" ]; then
-    echo "Update required to settings.php..."
-    if [ $settings_unmodified == true ]; then
-      sudo cp $emoncms_www/default.emonpi.settings.php $emoncms_www/settings.php
-      echo "settings.php autoupdated"
-    else
-      echo "**ERROR: unable to autoupdate settings.php since user changes are present, manual review required**"
-    fi
-  else
-    echo "settings.php not updated"
-  fi
 fi
 
 #########################################################################################
