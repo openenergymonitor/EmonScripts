6132f21b917ceb4cbe9327d4e2fcfcd0fa408e4f
Adrian Minde <adrian_minde@live.de>
2021-08-17 14:43:16 +0200

    Harden MySQL install

--- install/mysql.sh
+++ install/mysql.sh
@@ -11,15 +11,13 @@ echo "- Secure MYSQL"
 sudo mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1'); DELETE FROM mysql.user WHERE User=''; DROP DATABASE IF EXISTS test; DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'; FLUSH PRIVILEGES;"
 # Create the emoncms database using utf8 character decoding:
 echo "- Create $mysql_database database"
-sudo mysql -e "CREATE DATABASE $mysql_database DEFAULT CHARACTER SET utf8;"
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS $mysql_database DEFAULT CHARACTER SET utf8;"
 # Add emoncms database, set user permissions
 echo "- Add user:$mysql_user and assign to database:$mysql_database"
-sudo mysql -e "CREATE USER '$mysql_user'@'localhost' IDENTIFIED BY '$mysql_password';\
-GRANT ALL ON $mysql_database.* TO '$mysql_user'@'localhost';\
-FLUSH PRIVILEGES;"
+sudo mysql -e "CREATE USER IF NOT EXISTS '$mysql_user'@'localhost' IDENTIFIED BY '$mysql_password';"
+sudo mysql -e "GRANT ALL ON $mysql_database.* TO '$mysql_user'@'localhost';FLUSH PRIVILEGES;"
 
 if [ -z "$mysql_timeseries" ]; then
-    sudo mysql -e "CREATE DATABASE $mysql_timeseries DEFAULT CHARACTER SET utf8;\
-GRANT ALL ON $mysql_timeseries.* TO '$mysql_user'@'localhost';\
-FLUSH PRIVILEGES;"
+    sudo mysql -e "CREATE DATABASE IF NOT EXISTS $mysql_timeseries DEFAULT CHARACTER SET utf8;"
+    sudo mysql -e "GRANT ALL ON $mysql_timeseries.* TO '$mysql_user'@'localhost';FLUSH PRIVILEGES;"
 fi
