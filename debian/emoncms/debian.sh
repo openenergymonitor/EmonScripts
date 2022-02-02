3f7e6b32488601285fa1d8675f016dddabc9ce38
Adrian Minde <adrian_minde@live.de>
2022-02-02 15:04:07 +0100

    Fix core version number reading

--- debian/emoncms/debian.sh
+++ debian/emoncms/debian.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #Description: Build script to generate the emoncms debian package
+package_vers=$emoncms_version
 package_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 package_name="$(basename "$package_path")"
-package_id="$package_name"-"$emoncms_version"
+package_id="$package_name"-"$package_vers"
 package_build="$build_dir/$package_id"
 
 mkdir -p $package_build
