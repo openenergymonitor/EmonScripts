b6b08f0935e2fe37ad7f66acac3eb5cbe0fa99b9
Trystan Lea <trystan.lea@googlemail.com>
2022-11-02 17:36:47 +0000

    Install php8.1 as default

--- install/php.sh
+++ install/php.sh
@@ -4,21 +4,23 @@ source load_config.sh
 echo "-------------------------------------------------------------"
 echo "Install PHP"
 echo "-------------------------------------------------------------"
+if [ "$emonSD_pi_env" = "1" ]; then
+    curl https://packages.sury.org/php/apt.gpg | sudo tee /usr/share/keyrings/suryphp-archive-keyring.gpg >/dev/null
+    echo "deb [signed-by=/usr/share/keyrings/suryphp-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/sury-php.list
+    sudo apt update
+fi
 
-sudo apt-get install -y php
-
-# PHP_VER=$(php -v | head -n 1 | cut -d " " -f 2 | cut -f1-2 -d"." )
+sudo apt-get install -y php8.1
 
 # if [ "$install_apache" = true ]; then
 #     sudo apt-get install -y libapache2-mod-php
 # fi
 
 if [ "$install_mysql" = true ]; then
-    sudo apt-get install -y php-mysql
+    sudo apt-get install -y php8.1-mysql
 fi
 
-# sudo apt-get install -y php-gd php$PHP_VER-opcache php-curl php-pear php-dev php-mcrypt php-common php-mbstring
-sudo apt-get install -y php-gd php-curl php-pear php-dev php-common php-mbstring
+sudo apt-get install -y php8.1-gd php8.1-curl php-pear php8.1-dev php8.1-common php8.1-mbstring
 
 sudo pecl channel-update pecl.php.net
 
