0676b6744dd06a7a7a8221963f1121d6dda962dd
Brian Orpin <brian.orpin@gmail.com>
2023-02-04 19:29:39 +0000

    Update logrotate schema

--- update/emonsd.sh
+++ update/emonsd.sh
@@ -2,6 +2,14 @@
 source load_config.sh
 python_cmd=$1
 
+if [ ! -f /etc/logrotate.d/logrotate ]; then
+    echo "-------------------------------------------------------------"
+    echo "EmonPi Update logrotate"
+    echo "-------------------------------------------------------------"
+    sudo ln -sf $openenergymonitor_dir/EmonScripts/defaults/etc/logrotate.d/logrotate /etc/logrotate.d/logrotate
+    sudo chown root /etc/logrotate.d/logrotate
+fi
+
 echo "-------------------------------------------------------------"
 echo "EmonPi LCD Update"
 echo "-------------------------------------------------------------"
