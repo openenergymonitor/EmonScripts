3ba72026b2090f5b9e9c3a1b62cd7455c40b7c95
TrystanLea <trystan.lea@gmail.com>
2023-04-21 15:34:51 +0100

    get firmware download url from remote firmware_available

--- update/get_firmware_download_url.py
+++ update/get_firmware_download_url.py
@@ -3,16 +3,16 @@
 import sys
 import json
 
-with open("../firmware_available.json") as jsonFile:
-    firmware_available = json.load(jsonFile)
-    jsonFile.close()
-
-if len(sys.argv)==2:
-    firmware_key = sys.argv[1]
-    if firmware_key in firmware_available:
-        firmware = firmware_available[firmware_key]
-        print(firmware['download_url']+" "+str(firmware['baud'])+" "+str(firmware['version'])+" "+str(firmware['core']))
-        sys.exit(0)
+import urllib.request, json 
+with urllib.request.urlopen("https://raw.githubusercontent.com/openenergymonitor/EmonScripts/master/firmware_available.json") as url:
+    firmware_available = json.load(url)
+    
+    if len(sys.argv)==2:
+        firmware_key = sys.argv[1]
+        if firmware_key in firmware_available:
+            firmware = firmware_available[firmware_key]
+            print(firmware['download_url']+" "+str(firmware['baud'])+" "+str(firmware['version'])+" "+str(firmware['core']))
+            sys.exit(0)
 
 # Error
 print("firmware not found")
