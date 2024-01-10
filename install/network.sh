2a15ed929bd6fec00c2e28be5fb3bf8c2c0e6b24
Trystan Lea <trystan.lea@googlemail.com>
2024-01-10 15:50:39 +0000

    Update network.sh

--- install/network.sh
+++ install/network.sh
@@ -12,19 +12,19 @@ if [ ! -d $openenergymonitor_dir/emonpi ]; then
     git clone ${git_repo[emonpi]}
 fi
 
-cd /opt/emoncms/modules/network
-sudo ./install.sh
+# cd /opt/emoncms/modules/network
+# sudo ./install.sh
 
 # Wifi setup
-sudo ln -s $openenergymonitor_dir/emonpi/wifi-check /usr/local/bin/wifi-check
+# sudo ln -s $openenergymonitor_dir/emonpi/wifi-check /usr/local/bin/wifi-check
 
-sudo crontab -l > mycron
-if grep -Fq "wifi-check" mycron; then
-    echo "wifi-check already present in crontab"
-else
-    echo "* * * * * /usr/local/bin/wifi-check >> /var/log/emoncms/wificheck.log 2>&1" >> mycron
-    sudo crontab mycron
-    rm mycron
-fi
+#sudo crontab -l > mycron
+# if grep -Fq "wifi-check" mycron; then
+#     echo "wifi-check already present in crontab"
+# else
+#     echo "* * * * * /usr/local/bin/wifi-check >> /var/log/emoncms/wificheck.log 2>&1" >> mycron
+#     sudo crontab mycron
+#     rm mycron
+# fi
 
-echo "Network install complete, please reboot"
+# echo "Network install complete, please reboot"
