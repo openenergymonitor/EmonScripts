265b2ca14483815dbab91485622e468b67a6318b
TrystanLea <trystan.lea@gmail.com>
2024-01-31 21:59:16 +0000

    formatting

--- install/emonpilcd.sh
+++ install/emonpilcd.sh
@@ -9,13 +9,13 @@ cd $openenergymonitor_dir
 if [ ! -d $openenergymonitor_dir/emonPiLCD ]; then
     git clone -b $emonhub_branch ${git_repo[emonPiLCD]}
 else 
-    echo "- emonPiLCD repository already installed"
-    echo "- emonPiLCD running git pull:"
+    echo "-- emonPiLCD repository already installed"
+    echo "-- emonPiLCD running git pull:"
     git -C $openenergymonitor_dir/emonPiLCD pull
 fi
 
 if [ -f $openenergymonitor_dir/emonPiLCD/install.sh ]; then
     $openenergymonitor_dir/emonPiLCD/install.sh
 else
-    echo "ERROR: $openenergymonitor_dir/emonPiLCD/install.sh script does not exist"
+    echo "-- ERROR: $openenergymonitor_dir/emonPiLCD/install.sh script does not exist"
 fi
--- install/firmware.sh
+++ install/firmware.sh
@@ -6,15 +6,17 @@ source load_config.sh
 # --------------------------------------------------------------------------------
 cd $openenergymonitor_dir
 if [ ! -d $openenergymonitor_dir/avrdude-rpi ]; then
+    echo "-- installing avrdude-rpi"
     git clone ${git_repo[avrdude-rpi]}
     $openenergymonitor_dir/avrdude-rpi/install
 else 
-    echo "- avrdude-rpi already exists"
+    echo "-- avrdude-rpi already exists"
 fi
 
 cd $openenergymonitor_dir
 if [ ! -d $openenergymonitor_dir/RFM2Pi ]; then
+    echo "-- installing RFM2Pi"
     git clone ${git_repo[RFM2Pi]}
 else
-    echo "- RFM2Pi already exists"
+    echo "-- RFM2Pi already exists"
 fi
